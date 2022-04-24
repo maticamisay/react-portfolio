13c469b2d3503a16422634920635a27b3e8b6eec
maticamisay <maticamisay99@hotmail.com>
2022-04-24 18:36:45 -0300

    folder structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import App from './App'
+import './index.css'
+
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+
+root.render(<React.StrictMode>
+    <App />
+</React.StrictMode>);
