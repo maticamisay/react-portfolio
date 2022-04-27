1412c85983f7fc68f3d2b7289b963f580a149fa7
maticamisay <maticamisay99@hotmail.com>
2022-04-27 00:38:47 -0300

    experience component css

--- src/components/experience/ExperienceArticle.jsx
+++ src/components/experience/ExperienceArticle.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BsPatchCheckFill } from "react-icons/bs";
 
-const ExperienceArticle = ({name,experience}) => {
+const ExperienceArticle = ({ name, experience }) => {
   return (
     <article className="experience__details">
-      <BsPatchCheckFill />
-      <h4>{name}</h4>
-      <small className="text-light">{experience}</small>
+      <BsPatchCheckFill className="experience__details-icon"/>
+      <div>
+        <h4>{name}</h4>
+        <small className="text-light">{experience}</small>
+      </div>
     </article>
   );
 };
