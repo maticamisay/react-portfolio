dec34dda8cafd33d219460ca29807365738a1cc9
maticamisay-notebook <maticamisay99@hotmail.com>
2022-04-30 23:30:39 -0300

    portfolio y header css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio1.jpg";
+import IMG4 from "../../assets/portfolio2.jpg";
+import IMG5 from "../../assets/portfolio1.jpg";
+import IMG6 from "../../assets/portfolio2.jpg";
+import PortfolioItem from "./PortfolioItem";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section id="portfolio">
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG1}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG2}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG3}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG4}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG5}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+        <PortfolioItem
+          title="This is a portfolio title"
+          imgsrc={IMG6}
+          github="https://github.com"
+          demo="https://github.com"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
---
+++ src/components/portfolio/PortfolioItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PortfolioItem = ({ title, imgsrc, github, demo }) => {
+  return (
+    <article className="portfolio__item">
+      <div className="portfolio__item-image">
+        <img src={imgsrc} alt="" />
+      </div>
+      <h3>{title}</h3>
+      <div className="portfolio__item-cta">
+        <a href={github} className="btn btn-primary">
+          Github
+        </a>
+        <a href={demo} className="btn btn-primary" target="_blank">
+          Live Demo
+        </a>
+      </div>
+    </article>
+  );
+};
+
+export default PortfolioItem;
