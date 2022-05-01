992b38e521fcf931bdd9b09a9223ab8f87a8dc08
maticamisay-notebook <maticamisay99@hotmail.com>
2022-05-01 01:59:14 -0300

    testimonials css y general css changes

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import Avatar1 from "../../assets/avatar.png";
+
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Avatar1} alt="avatar png" />
+          </div>
+          <h5 className="client__name">Nombre</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+            corporis nostrum sapiente deleniti laborum ratione inventore totam
+            quos cum eaque?
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Avatar1} alt="avatar png" />
+          </div>
+          <h5 className="client__name">Nombre</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+            corporis nostrum sapiente deleniti laborum ratione inventore totam
+            quos cum eaque?
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Avatar1} alt="avatar png" />
+          </div>
+          <h5 className="client__name">Nombre</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+            corporis nostrum sapiente deleniti laborum ratione inventore totam
+            quos cum eaque?
+          </small>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
