89a57b4aa6d3b535a0b02df6373801b92b4eab3e
maticamisay <maticamisay99@hotmail.com>
2022-05-01 16:46:28 -0300

    header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,11 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Matias</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <div className="header__text">
+          <h5>Hello I'm</h5>
+          <h1>Matias</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+        </div>
         <CTA />
         <HeaderSocials />
         <div className="me">
