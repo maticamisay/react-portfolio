12d428b7ec879e404b185ec62e0dee50d2dc7538
maticamisay <maticamisay99@hotmail.com>
2022-05-04 19:23:02 -0300

    footer component y css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Matias Camisay
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
 
-export default Footer
+      <div className="footer__copyright">
+        <small>&copy; Matias Camisay. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
