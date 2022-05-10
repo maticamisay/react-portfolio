c4d3d3359b1b6ec9658ccfc0c5dc5b2bb16d1003
maticamisay <maticamisay99@hotmail.com>
2022-05-09 22:13:55 -0300

    services traduction

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import "./services.css";
 import ServiceCard from "./ServiceCard";
+import { useTranslation } from "react-i18next";
+
 
 const Services = () => {
+  const [t, i18n] = useTranslation();
+
   return (
     <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>{t("services.what")}</h5>
+      <h2>{t("services.Services")}</h2>
       <div className="container services__container">
         <ServiceCard
-          title="UX/UI Designer"
+          title={t("services.ux")}
           service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service4="Lorem ipsum dolor sit amet, consectetur adipisicing."
         />
         <ServiceCard
-          title="Web Development"
+          title={t("services.web")}
           service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service4="Lorem ipsum dolor sit amet, consectetur adipisicing."
         />
         <ServiceCard
-          title="Content Creation"
+          title={t("services.content")}
           service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
           service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
