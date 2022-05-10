81ab4d4622ca35dd243b273999f8bfd83816b319
maticamisay <maticamisay99@hotmail.com>
2022-05-10 20:30:22 -0300

    services update

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsCheck } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const ServiceCard = ({ title, service1, service2, service3, service4 }) => {
   return (
@@ -9,24 +10,28 @@ const ServiceCard = ({ title, service1, service2, service3, service4 }) => {
       </div>
       <ul className="service__list">
         <li>
-          <BsCheck className="service__list-icon" />
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsCheck className="service__list-icon" />
+          </IconContext.Provider>
           <p>{service1}</p>
         </li>
         <li>
-          <BsCheck className="service__list-icon" />
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsCheck className="service__list-icon" />
+          </IconContext.Provider>
           <p>{service2}</p>
         </li>
         <li>
-          <BsCheck className="service__list-icon" />
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsCheck className="service__list-icon" />
+          </IconContext.Provider>
           <p>{service3}</p>
         </li>
         <li>
-          <BsCheck className="service__list-icon" />
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsCheck className="service__list-icon" />
+          </IconContext.Provider>
           <p>{service4}</p>
-        </li>
-        <li>
-          <BsCheck className="service__list-icon" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
         </li>
       </ul>
     </article>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -14,24 +14,24 @@ const Services = () => {
       <div className="container services__container">
         <ServiceCard
           title={t("services.ux")}
-          service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service4="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          service1={t("services.ux1")}
+          service2={t("services.ux2")}
+          service3={t("services.ux3")}
+          service4={t("services.ux4")}
         />
         <ServiceCard
           title={t("services.web")}
-          service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service4="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          service1={t("services.web1")}
+          service2={t("services.web2")}
+          service3={t("services.web3")}
+          service4={t("services.web4")}
         />
         <ServiceCard
           title={t("services.content")}
-          service1="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service2="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service3="Lorem ipsum dolor sit amet, consectetur adipisicing."
-          service4="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          service1={t("services.cont1")}
+          service2={t("services.cont2")}
+          service3={t("services.cont3")}
+          service4={t("services.cont4")}
         />
       </div>
     </section>
