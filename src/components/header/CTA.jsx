946947192f407812c16892b34c99201deea04837
maticamisay <maticamisay99@hotmail.com>
2022-05-10 23:37:33 -0300

    change cv pdf according to the lang

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf'
+import React from "react";
+// import CV from '../../assets/CV.pdf'
+import CV_Eng from "../../assets/CV_English.pdf";
+import CV_Esp from "../../assets/CV_Esp.pdf";
 import { useTranslation } from "react-i18next";
 
 const CTA = () => {
   const [t, i18n] = useTranslation();
-
+  let CV = t("header.lang");
 
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>{t("header.boton_cv")}</a>
-        <a href='#contact' className='btn btn-primary'>{t("header.boton_contacto")}</a>
+    <div className="cta">
+      {CV == "LANGUAGE" ? (
+        <a href={CV_Esp} download className="btn">
+          {t("header.boton_cv")}
+        </a>
+      ) : (
+        <a href={CV_Eng} download className="btn">
+          {t("header.boton_cv")}
+        </a>
+      )}
+      {/* <a href={CV} download className="btn">
+        {t("header.boton_cv")}
+      </a> */}
+      <a href="#contact" className="btn btn-primary">
+        {t("header.boton_contacto")}
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
